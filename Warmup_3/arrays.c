1df21da24ee5e9d09eaf2fd400ef184d803ef4f8
david ludington <diludington@gmail.com>
2024-01-23 19:04:42 +0000

    updated arrays.c

--- Warmup_3/arrays.c
+++ Warmup_3/arrays.c
@@ -4,12 +4,13 @@
 int main(int argc, int * * argv){
     int numbers[] = {1, 2, 3, 4, 5};
     int sum = 0;
+    int length = *(&numbers + 1) - numbers;
 
-    for(int i = 0; i < 5; i++){
+    for(int i = 0; i < length; i++){
         sum+= numbers[i];
     }
 
-    double average = sum / (double)5;
+    double average = sum / (double)length;
 
     printf("Sum: %d\n", sum);
     printf("Average: %lf\n", average);
