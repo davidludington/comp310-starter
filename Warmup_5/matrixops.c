74697233a83f344a5b5c5ea2ae131bd27e5f1677
david ludington <diludington@gmail.com>
2024-01-23 19:13:48 +0000

    Warmup_5

---
+++ Warmup_5/matrixops.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, int * * argv){
+    int[][] matrix1 = {{1,2},{3,4}};
+    int[][] matrix2 = {{5,6},{7,8}};
+
+    int sum = add_Matricies(matrix1, matrix2);
+    int product = multiply_Matricies(matix1, matrix2);
+
+
+}
+
+int[][] add_Matricies(int[][] a, int [][] b){
+    int rows = 
+}
