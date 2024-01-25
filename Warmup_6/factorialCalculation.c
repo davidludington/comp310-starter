9c77d03312bd73a169ad992f479a31168cf28a00
david ludington <diludington@gmail.com>
2024-01-25 18:46:19 +0000

    Factorial calculation

---
+++ Warmup_6/factorialCalculation.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int factorial(int num){
+    int product = 1;
+
+    if(num <= 0){
+        return 1;
+    }
+
+    while(num > 0){
+        product = product* num;
+        num--;
+    }
+    return product;
+}
+
+
+int main(int argc, int **argv){
+    int number = 8;
+
+    printf("The factoral of %d is %d\n", number, factorial(number));
+}
