9b479123c3354ccbb8a9552063b960b96d304b79
david ludington <diludington@gmail.com>
2024-01-28 19:38:20 +0000

    file operations

--- Warmup_9/fileops.c
+++ Warmup_9/fileops.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 int main(int argc, char ** argv){
 
+    FILE *fptr;
+
+    //open file to write to
+    fptr = fopen("example.txt", "w");
+    if(fptr == NULL){
+        printf("error\n");
+        exit(1);
+    }
+    fprintf(fptr, "%s", "hello world");
+    fclose(fptr);
+    
+    
+    //read from file char by char
+    fptr = fopen("example.txt", "r");
+    char c;
+    c = fgetc(fptr);
+    if(fptr == NULL){
+        printf("error\n");
+        exit(1);
+    }
+    while(c != EOF){
+        printf("%c", c);
+        c = fgetc(fptr);
+    }
+    printf("\n");
+    fclose(fptr);
+
+    return 0;
+
+     
 }
