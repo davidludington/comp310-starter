94bf9cca0e07a9f0fc7347adc4b9a3237e75dd50
david ludington <diludington@gmail.com>
2024-01-30 17:27:43 +0000

    realized I was not supposed to use library functions

--- Warmup_4/string_reversal.c
+++ Warmup_4/string_reversal.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-#include <string.h>
+//#include <string.h>
 
 
 int main(int argc, int * * argv){
     char str[] = "Hello";
+    int length = sizeof(str)/sizeof(str[0]) - 1;
 
-    for(int i = 0, j = strlen(str) - 1; i < j; i++, j--){
+    for(int i = 0, j = length -1; i < j; i++, j--){
         char temp = str[i];
         str[i] = str[j];
         str[j] = temp;
